1696c4b414e0c7f9b5a3f782166ed30fda128ddb
kevinxin90 <kevinxin@scripps.edu>
2017-11-29 10:16:18 -0800

    add call api function

--- biothings_explorer/api_call_handler.py
+++ biothings_explorer/api_call_handler.py
@@ -30,4 +30,35 @@ class ApiCallHandler:
         else:
             return False
 
-    
+
+    def call_api(self, uri_value, endpoint_name):
+        """
+        construct requests params/data, based on input type and value
+        only handle 'in' value which is body or query
+        uri_value is a dict constituting uri:value pairs
+        """
+        results = {}
+        # temp holder for method, should extend this function to handle 'post'
+        method = 'get'
+        for _para in self.registry.endpoint_info[endpoint_name][method]['parameters']:
+            # handle cases where input value is part of the url
+            if _para['in'] == 'path':
+                endpoint_name = endpoint_name.replace('{' + _para['name'] + '}', str(uri_value[_para['x-valueType'][0]]))
+            # handle cases for query
+            else:
+                # check whether the parameter is required
+                if _para['required']:
+                    # if the para has a request template, then put value into the placeholder {{input}}
+                    if 'x-requestTemplate' in _para:
+                        for _template in _para['x-requestTemplate']:
+                            if _template['valueType'] == 'default':
+                                results[_para['name']] = _template['template'].replace('{{input}}', json.dumps(list(uri_value.values())[0]))
+                            elif _template['valueType'] in uri_value.keys():
+                                results[_para['name']] = _template['template'].replace('{{input}}', json.dumps(uri_value[_template['valueType']]))
+                    else:
+                        results[_para['name']] = value
+        if requests.get(endpoint_name, params=results).status_code -- 200:
+            return requests.get(endpoint_name, params=results)
+        else:
+            print('This API call returns no results. The URI given is {}, the endpoint given is {}'.format(uri_value, endpoint_name))
+            return
