88f46488543fcb2ea87e2b116bcbeb0eeb50364b
kevinxin90 <kevinxin@scripps.edu>
2017-11-29 15:46:17 -0800

    add draw api road map fucntion

--- biothings_explorer/__init__.py
+++ biothings_explorer/__init__.py
@@ -4,11 +4,15 @@ import networkx as nx
 from api_call_handler import ApiCallHandler
 
 class BioThingsExplorer:
-    def __init__(self, initialize=True):
+    def __init__(self, loadroadmap=True):
         self.graph_id = 1
         self.apiCallHandler = ApiCallHandler()
         self.registry = self.apiCallHandler.registry
         self.api_map = nx.MultiGraph()
+        self.temp_G = nx.MultiGraph()
+        self.graph_id = 0
+        if loadroadmap:
+            self.construct_api_road_map()
 
     def show_available_bioentities(self):
         """
@@ -32,18 +36,27 @@ class BioThingsExplorer:
             self.api_map.add_node(_api, type='api', color='red')
             for _endpoint in _info['endpoints']:
                 self.api_map.add_node(_endpoint, type='endpoint', color='blue')
-                self.api_map.add_edge(_api, _endpoint, label='has_endpoint')
+                self.api_map.add_edge(_endpoint, _api, label='has_endpoint')
         # add endpoint and input/output to the graph
         for _endpoint, _info in self.registry.endpoint_info.items():
             for _input in _info['input']:
                 preferred_name = self.registry.bioentity_info[_input]['preferred_name']
                 self.api_map.add_node(preferred_name, type='bioentity', color='yellow')
-                self.api_map.add_edge(preferred_name, _endpoint, label='has_input')
+                self.api_map.add_edge(_endpoint, preferred_name, label='has_input')
             for _output in _info['output']:
                 preferred_name = self.registry.bioentity_info[_output]['preferred_name']
                 self.api_map.add_node(preferred_name, type='bioentity', color='yellow')
                 relations = _info['relation'][_output]
                 for _relation in relations:
-                    self.api_map.add_edge(_endpoint, preferred_name, label=_relation)
+                    self.api_map.add_edge(preferred_name, _endpoint, label=_relation)
         return self.api_map
+
+    def draw_api_road_map(self):
+        """
+        This function displays the api road map in the jupyter notebook cell block
+        """
+        if not self.api_map.nodes():
+            print('Please call construct_api_road_map function first!')
+            return
+        return draw_graph(self.api_map, graph_id=self.graph_id = self.graph_id + 1)
 
