366019c05fa41049fb44de1b9c7c96b9bce5f2f8
kevinxin90 <kevinxin@scripps.edu>
2018-07-11 22:38:54 -0700

    update setup file

--- setup.py
+++ setup.py
@@ -7,17 +7,15 @@ def read(fname):
 
 
 setup(
-    name="biothings_explorer",
-    version="0.2.0",
+    name="biothings_explorer_test",
+    version="0.3.0",
     author="Jiwen Xin, Chunlei Wu",
     author_email="cwu@scripps.edu",
     description="Python Client for BioThings Explorer",
     license="BSD",
     keywords="link API interoperability biology explore",
     url="https://github.com/biothings/biothings_explorer",
-    packages=['biothings_explorer'],
-    package_data={'biothings_explorer': ['context']},
-    include_package_data=True,
+    packages=['biothings_explorer_test'],
     classifiers=[
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.7",
@@ -37,12 +35,6 @@ setup(
         "Topic :: Scientific/Engineering :: Bio-Informatics",
     ],
     install_requires=[
-        'requests>=2.3.0',
-        "PyLD>=0.7.2",
-        "visJS2jupyter>=0.1.8",
-        "networkx>=2.0",
-        "pandas>=0.21.0",
-        "tabulate>=0.8.2",
-        "ipython>=6.2.1"
-    ],
+        'requests>=2.3.0'
+    ]
 )
